a812319de9820eebf72b29cd2ce5da04855c6250
Git-Axo <124710371+Git-Axo@users.noreply.github.com>
2026-03-22 07:05:02 +0000

    Add database setup script and LAMP stack instructions

---
+++ lamp_setup.sh
@@ -0,0 +1,7 @@
+# Runs the db_setup.sql script to set up the database.
+sudo service mariadb start
+echo "MariaDB started"
+sudo mariadb -u root < db_setup.sql
+echo "Database loaded"
+sudo service apache2 start
+echo "Apache started"
